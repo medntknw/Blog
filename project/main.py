87f9c1d2eff1c23e7019897d6eb59e4baea794e0
Aniket <aniket.patanwal99@gmail.com>
2021-01-25 00:32:16 +0530

    Added features

--- project/main.py
+++ project/main.py
@@ -1,12 +1,28 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, redirect, url_for, request,flash
 from flask_login import login_required, current_user
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User, Blogs
 from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    posts = Blogs.query.limit(10).all()
+    return render_template('index.html', data=posts)
+
+@login_required
+@main.route('/', methods=['POST'])
+def index_post():
+    post = request.form.get('post')
+    email = current_user.email
+    new_post = Blogs(email_id=email,post=post)
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+
 
 @login_required
 @main.route('/profile')
--- project/models.py
+++ project/models.py
@@ -6,3 +6,10 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    posts = db.relationship('Blogs', backref='name', lazy=True)
+
+class Blogs(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post = db.Column(db.String(4096))
+    email_id = db.Column(db.String(100),db.ForeignKey(User.email))
+    
